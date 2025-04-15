8111d02c9b421bf3e97402288c5aed01ae897a1a
clayton <clayton.durepos@maine.edu>
2025-04-14 23:45:28 -0400

    Removed sigmoid() activation

--- classification_models.py
+++ classification_models.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.nn import BCEWithLogitsLoss
 from transformers import AutoModel
 
 
@@ -8,8 +9,7 @@ class MLPHeader(nn.Module):
         self.model = nn.Sequential(
             nn.Linear(dim, dim // 4),
             nn.GELU(),
-            nn.Linear(dim // 4, num_classes),
-            nn.Sigmoid()
+            nn.Linear(dim // 4, num_classes)
         )
 
     def forward(self, x):
@@ -21,12 +21,19 @@ class TextClassifier(nn.Module):
         self.base = AutoModel.from_pretrained(base_model)
         self.classifier = MLPHeader(self.base.config.hidden_size, num_classes)
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, labels=None):
         pooled = self.base(input_ids=input_ids,
                            attention_mask=attention_mask,
                            token_type_ids=token_type_ids
                            ).pooler_output
-        return self.classifier(pooled)
+
+        logits = self.classifier(pooled)
+        if labels is not None:
+            loss_fn = BCEWithLogitsLoss()
+            loss = loss_fn(logits, labels)
+            return {"logits": logits, "loss": loss}
+
+        return {"logits": logits}
 
 class ImageClassifier(nn.Module):
     def __init__(self, base_model:str, num_classes):
@@ -34,6 +41,13 @@ class ImageClassifier(nn.Module):
         self.base = AutoModel.from_pretrained(base_model)
         self.classifier = MLPHeader(self.base.config.hidden_size, num_classes)
 
-    def forward(self, pixel_values):
+    def forward(self, pixel_values, labels=None):
         pooled = self.base(pixel_values=pixel_values.squeeze(1)).pooler_output
-        return self.classifier(pooled)
+
+        logits = self.classifier(pooled)
+        if labels is not None:
+            loss_fn = BCEWithLogitsLoss()
+            loss = loss_fn(logits, labels)
+            return {"logits": logits, "loss": loss}
+
+        return {"logits": logits}
